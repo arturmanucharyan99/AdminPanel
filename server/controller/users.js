a678c88d885f1716e909a87e89a5a8a32d9abe1d
arturmanucharyan99 <arturmanucharyan99@gmail.com>
2023-11-21 15:52:08 +0400

    validateFix

--- server/controller/users.js
+++ server/controller/users.js
@@ -17,7 +17,7 @@ class UsersController {
     async userPost(req, res, next) {
         const { firstName, lastName, userName, email, password } = req.body;
         const errors = validationResult(req);
-
+        console.log(errors);
         if (!errors.isEmpty()) {
             return res.status(422).json(
                 {
--- server/middleware/userValidation.js
+++ server/middleware/userValidation.js
@@ -2,8 +2,8 @@ import { body, check } from "express-validator";
 import Users from "../model/users.js";
 
 export const userValidator = [
-   body('firstName').isString().isLength({ min: 3, max: 25 }).notEmpty().withMessage('Please enter min 3 and max 25 symbols').trim(),
-   body('lastName').isString().isLength({ min: 3, max: 25 }).notEmpty().withMessage('Please enter min 3 and max 25 symbols').trim(),
+   body('firstName').isString().isLength({ min: 3, max: 25 }).withMessage('Please enter min 3 and max 25 symbols').notEmpty().trim(),
+   body('lastName').isString().isLength({ min: 3, max: 25 }).withMessage('Please enter min 3 and max 25 symbols').notEmpty().trim(),
    body('userName').isString().isLength({ min: 3, max: 25 }).notEmpty().trim().custom(async (value, { req }) => {
       const user = await Users.findOne({ userName: value })
       if (user) {
